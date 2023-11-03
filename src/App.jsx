9378b8233493e56a83e72611621650d327e0af61
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-11-03 11:54:28 +0100

    La synthaxe JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 function App() {
-    return 'Bonjour les gens';
+    const myList = ['Item 1', 'Item 2', 'Item 3'];
+
+    return (
+        <>
+            <Title id="myId" className="myClass" color="blue">
+                Mon premier composant
+            </Title>
+            <ul>
+                {myList.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+function Title({ color, hidden, children, ...props }) {
+    if (hidden) {
+        return null;
+    }
+
+    return (
+        <h1 style={{ color: color }} {...props}>
+            {children}
+        </h1>
+    );
 }
 
 export default App;
