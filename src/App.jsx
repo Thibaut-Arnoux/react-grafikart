3af2ee6db42be0c37d525b04604ef4f41779f162
Thibaut-Arnoux <thib.arnoux@gmail.com>
2023-11-21 11:45:50 +0100

    Les portails dans React

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
-import { memo, useCallback, useRef, useState } from 'react';
-import { Input } from './components/forms/Input';
+import { createPortal } from 'react-dom';
 
 function App() {
-    console.log('App', 'render');
-
-    const [name, setName] = useState('');
-    const nameRef = useRef(name);
-    nameRef.current = name;
-
-    const handleClick = useCallback(() => {
-        console.log(nameRef.current);
-    }, []);
-
     return (
-        <div>
-            <div>
-                <Input label="Prénom" onChange={setName} value={name} />
-                <div>{name.toUpperCase()}</div>
-            </div>
-            <InfoMemo onClick={handleClick} />
+        <div
+            style={{
+                height: 300,
+                overflowY: 'scroll',
+                background: '#EEE',
+                margin: 20,
+                position: 'relative'
+            }}
+        >
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe obcaecati temporibus
+                laboriosam officiis excepturi vero maiores sequi, repudiandae beatae atque laborum.
+                Corporis, magnam perspiciatis nobis nesciunt esse totam accusantium ullam!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eius laboriosam, autem
+                beatae amet nihil laborum vitae aut unde officiis tempora, quos sed obcaecati iusto
+                quaerat cumque, impedit enim quasi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, placeat dolor quis
+                ex esse vero blanditiis quibusdam? Accusamus nam minima debitis vel perspiciatis
+                dolor sed expedita, sequi consequuntur cumque ea?
+            </p>
+            <Modal />
         </div>
     );
 }
 
-const InfoMemo = memo(function Info({ onClick }) {
-    console.log('Info', 'render');
-
-    return (
-        <div className="alert alert-info" onClick={onClick}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias delectus numquam
-            voluptas velit veritatis
-        </div>
+function Modal() {
+    return createPortal(
+        <div
+            style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                padding: 10,
+                border: 'solid 1px grey',
+                backgroud: '#FFF'
+            }}
+        >
+            Je suis une modal
+        </div>,
+        document.body
     );
-});
+}
 
 export default App;
