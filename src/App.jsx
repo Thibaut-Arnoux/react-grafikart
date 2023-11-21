e60687fdbd4fb854198dd7ac157ea8bd08a66107
ThibautArnoux <thibaut.arnoux@kairnialgroup.com>
2023-11-21 11:34:12 +0100

    Mémoisation et useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,37 @@
-import { useState } from 'react';
+import { memo, useCallback, useRef, useState } from 'react';
 import { Input } from './components/forms/Input';
-import { useIncrement } from './hooks/useIncrement';
-import { useToggle } from './hooks/useToggle';
-import { useDocumentTitle } from './hooks/useDocumentTitle';
-import { useFetch } from './hooks/useFetch';
 
 function App() {
-    const [checked, setChecked] = useToggle();
-    const { count, increment, decrement } = useIncrement({
-        base: 0,
-        min: -10,
-        max: 10
-    });
-    const [title, setTitle] = useState('');
-    useDocumentTitle(title ? `Editer ${title}` : null);
+    console.log('App', 'render');
 
-    const { data, loading, errors } = useFetch(
-        'https://jsonplaceholder.typicode.com/posts?_limit=10&_delay=5000'
-    );
+    const [name, setName] = useState('');
+    const nameRef = useRef(name);
+    nameRef.current = name;
+
+    const handleClick = useCallback(() => {
+        console.log(nameRef.current);
+    }, []);
 
     return (
         <div>
-            <input type="checkbox" checked={checked} onChange={setChecked} />
-            <hr />
-            <button onClick={increment}>+</button>
-            <span>{count}</span>
-            <button onClick={decrement}>-</button>
-            {checked && (
-                <>
-                    <hr />
-                    <Input value={title} onChange={setTitle} label="Title" />
-                </>
-            )}
-            <hr />
-            <div className="container my-2">
-                {loading && (
-                    <div className="spinner-border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </div>
-                )}
-                {errors && <div className="alert alert-danger">{errors.toString()}</div>}
-                {data && (
-                    <div>
-                        <ul>
-                            {data.map((post) => (
-                                <li key={post.id}>{post.title}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+            <div>
+                <Input label="Prénom" onChange={setName} value={name} />
+                <div>{name.toUpperCase()}</div>
             </div>
+            <InfoMemo onClick={handleClick} />
         </div>
     );
 }
+
+const InfoMemo = memo(function Info({ onClick }) {
+    console.log('Info', 'render');
+
+    return (
+        <div className="alert alert-info" onClick={onClick}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias delectus numquam
+            voluptas velit veritatis
+        </div>
+    );
+});
 
 export default App;
